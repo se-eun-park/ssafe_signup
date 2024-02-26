cfc709a847adfe146e6c85200ecc8c3f15cfdcb5
박세은 <seeunbag30@gmail.com>
2024-02-27 01:49:15 +0900

    Feat: 비밀번호 확인 유효성 검사

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,6 +6,7 @@ const SignUpPage = () => {
     register,
     formState: { errors },
     handleSubmit,
+    getValues,
   } = useForm();
 
   // 유효성 검사
@@ -13,7 +14,7 @@ const SignUpPage = () => {
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,16}$/;
 
-  const onSubmit = (form) => console.log(form.required);
+  const onSubmit = (data) => console.log(data.required);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -34,25 +35,44 @@ const SignUpPage = () => {
         <p>이메일 양식에 맞게 입력해주세요</p>
       )}
 
-      <label htmlFor="pw">비밀번호</label>
+      <label htmlFor="password">비밀번호</label>
       <input
-        id="pw"
+        id="password"
         type="password"
-        placeholder="PW"
-        {...register("pw", {
+        placeholder="PW(영문, 숫자, 특수문자 포함 8자 ~ 16자)"
+        {...register("password", {
           required: true,
-          minLength: 8,
+          pattern: passwordRegex,
         })}
       />
-      {errors.pw && errors.pw.type === "required" && (
+      {errors.password && errors.password.type === "required" && (
         <p>비밀번호를 입력해주세요</p>
       )}
-      {errors.pw && errors.pw.type === "minLength" && (
+      {errors.password && errors.password.type === "pattern" && (
         <p>
-          영문자, 숫자, 특수문자를 하나 이상 포함한 8자리 이상 16자리 이하의
+          영문자, 숫자, 특수문자를 하나 이상 포함한 8자리 이상, 16자리 이하의
           비밀번호여야 합니다.
         </p>
       )}
+
+      <label htmlFor="passwordCheck">비밀번호 확인</label>
+      <input
+        id="passwordCheck"
+        type="password"
+        placeholder="비밀번호 확인"
+        {...register("passwordCheck", {
+          required: "비밀번호를 확인해주세요",
+          validate: {
+            matchPassword: (value) => {
+              const { password } = getValues();
+              return password === value || "비밀번호가 일치하지 않습니다";
+            },
+          },
+        })}
+      />
+      {errors.passwordCheck && <p>{errors.passwordCheck.message}</p>}
+
+      <button type="submit">회원가입</button>
     </form>
   );
 };
