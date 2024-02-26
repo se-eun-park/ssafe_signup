961ed61b82c27bbd407fc0ed35bcd6d4889e4402
박세은 <seeunbag30@gmail.com>
2024-02-27 01:23:00 +0900

    Feat: 비밀번호 유효성 검사

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -33,6 +33,26 @@ const SignUpPage = () => {
       {errors.email && errors.email.type === "pattern" && (
         <p>이메일 양식에 맞게 입력해주세요</p>
       )}
+
+      <label htmlFor="pw">비밀번호</label>
+      <input
+        id="pw"
+        type="password"
+        placeholder="PW"
+        {...register("pw", {
+          required: true,
+          minLength: 8,
+        })}
+      />
+      {errors.pw && errors.pw.type === "required" && (
+        <p>비밀번호를 입력해주세요</p>
+      )}
+      {errors.pw && errors.pw.type === "minLength" && (
+        <p>
+          영문자, 숫자, 특수문자를 하나 이상 포함한 8자리 이상 16자리 이하의
+          비밀번호여야 합니다.
+        </p>
+      )}
     </form>
   );
 };
