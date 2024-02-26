92763c8122c4e292d00a57202fe2692d7cf990fb
박세은 <seeunbag30@gmail.com>
2024-02-27 00:14:12 +0900

    Feat: 페이지 라우팅
    App.js에 react-router-dom 을 사용해 Home, SignUp 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
