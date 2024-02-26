a44081c383a649ae307b37466afe7dacf1bb242a
박세은 <seeunbag30@gmail.com>
2024-02-27 01:17:28 +0900

    Feat: 이메일 유효성 검사

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const SignUpPage = () => {
-  return <h1>SignUp</h1>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  // 유효성 검사
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,16}$/;
+
+  const onSubmit = (form) => console.log(form.required);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="email">이메일</label>
+      <input
+        id="email"
+        type="text"
+        placeholder="Email"
+        {...register("email", {
+          required: true,
+          pattern: emailRegex,
+        })}
+      />
+      {errors.email && errors.email.type === "required" && (
+        <p>이메일을 입력해주세요</p>
+      )}
+      {errors.email && errors.email.type === "pattern" && (
+        <p>이메일 양식에 맞게 입력해주세요</p>
+      )}
+    </form>
+  );
 };
 
 export default SignUpPage;
