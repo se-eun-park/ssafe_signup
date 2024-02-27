c72be53c81a3b2f9d4f7ddb1d2912f00521cb7fa
세은 <seeunbag30@gmail.com>
2024-02-27 15:30:12 +0900

    Design: SingUpPage CSS 작업

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SignUpButton = () => {
   return (
-    <button className="transition-all bg-[#6ED1F9] w-72 h-14 text-2xl font-noto-sans-kr text-white rounded-full hover:bg-white border-[3px] border-[#6ED1F9] hover:text-[#6ED1F9]">
+    <button className="transition-all bg-[#6ED1F9] w-96 h-14 text-2xl font-noto-sans-kr text-white rounded-full hover:bg-white border-[3px] border-[#6ED1F9] hover:text-[#6ED1F9]">
       회원가입
     </button>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import SignUpButton from "../components/SignUpButton";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
@@ -24,61 +25,79 @@ const SignUpPage = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email">이메일</label>
-      <input
-        id="email"
-        type="text"
-        placeholder="Email"
-        {...register("email", {
-          required: true,
-          pattern: emailRegex,
-        })}
-      />
-      {errors.email && errors.email.type === "required" && (
-        <p>이메일을 입력해주세요</p>
-      )}
-      {errors.email && errors.email.type === "pattern" && (
-        <p>이메일 양식에 맞게 입력해주세요</p>
-      )}
-
-      <label htmlFor="password">비밀번호</label>
-      <input
-        id="password"
-        type="password"
-        placeholder="PW(영문, 숫자, 특수문자 포함 8자 ~ 16자)"
-        {...register("password", {
-          required: true,
-          pattern: passwordRegex,
-        })}
-      />
-      {errors.password && errors.password.type === "required" && (
-        <p>비밀번호를 입력해주세요</p>
-      )}
-      {errors.password && errors.password.type === "pattern" && (
-        <p>
-          영문자, 숫자, 특수문자를 하나 이상 포함한 8자리 이상, 16자리 이하의
-          비밀번호여야 합니다.
-        </p>
-      )}
-
-      <label htmlFor="passwordCheck">비밀번호 확인</label>
-      <input
-        id="passwordCheck"
-        type="password"
-        placeholder="비밀번호 확인"
-        {...register("passwordCheck", {
-          required: "비밀번호를 확인해주세요",
-          validate: {
-            matchPassword: (value) => {
-              const { password } = getValues();
-              return password === value || "비밀번호가 일치하지 않습니다";
-            },
-          },
-        })}
-      />
-      {errors.passwordCheck && <p>{errors.passwordCheck.message}</p>}
-
-      <button type="submit">회원가입</button>
+      <div className="flex w-full h-screen justify-center items-center">
+        <div className="flex flex-col w-[400px] h-[508px] justify-around">
+          <div className="flex flex-col">
+            <label className="labelText" htmlFor="email">
+              이메일
+            </label>
+            <input
+              className="inputField"
+              id="email"
+              type="text"
+              placeholder="Email"
+              {...register("email", {
+                required: true,
+                pattern: emailRegex,
+              })}
+            />
+            {errors.email && errors.email.type === "required" && (
+              <p className="errorMessage">이메일을 입력해주세요</p>
+            )}
+            {errors.email && errors.email.type === "pattern" && (
+              <p className="errorMessage">이메일 양식에 맞게 입력해주세요</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label className="labelText" htmlFor="password">
+              비밀번호
+            </label>
+            <input
+              className="inputField"
+              id="password"
+              type="password"
+              placeholder="영문, 숫자, 특수문자 포함 8자 ~ 16자"
+              {...register("password", {
+                required: true,
+                pattern: passwordRegex,
+              })}
+            />
+            {errors.password && errors.password.type === "required" && (
+              <p className="errorMessage">비밀번호를 입력해주세요</p>
+            )}
+            {errors.password && errors.password.type === "pattern" && (
+              <p className="errorMessage">
+                영문자, 숫자, 특수문자를 하나 이상 포함한 8자리 이상, 16자리
+                이하의 비밀번호여야 합니다.
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label className="labelText" htmlFor="passwordCheck">
+              비밀번호 확인
+            </label>
+            <input
+              className="inputField"
+              id="passwordCheck"
+              type="password"
+              placeholder="비밀번호를 다시 입력해 주세요"
+              {...register("passwordCheck", {
+                required: "비밀번호를 확인해주세요",
+                validate: {
+                  matchPassword: (value) => {
+                    const { password } = getValues();
+                    return password === value || "비밀번호가 일치하지 않습니다";
+                  },
+                },
+              })}
+            />
+            {errors.passwordCheck && (
+              <p className="errorMessage">{errors.passwordCheck.message}</p>
+            )}
+          </div>
+          <SignUpButton type="submit" />
+        </div>
+      </div>
     </form>
   );
 };
