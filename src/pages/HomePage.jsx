6558dc86058953a272782c1a92b4d7dde55cc588
세은 <seeunbag30@gmail.com>
2024-02-27 09:26:40 +0900

    Feat: Home에서 버튼을 누르면 SignUp으로 이동

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SignUpButton from "../components/SignUpButton";
 
 const HomePage = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Link to="signup">
+        <SignUpButton />
+      </Link>
+    </>
+  );
 };
 
 export default HomePage;
