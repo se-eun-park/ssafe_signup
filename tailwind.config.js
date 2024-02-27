590a53c000c47a0908f3085685c0003b9c393e30
세은 <seeunbag30@gmail.com>
2024-02-27 12:31:59 +0900

    Docs: google font link

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "noto-sans-kr": ['"Noto Sans KR"', "sans-serif"],
+    },
   },
   plugins: [],
 };
